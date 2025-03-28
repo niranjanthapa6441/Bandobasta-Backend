dbafc7d0ab9250254b6eb68468caac461637acf5
Anurag Nepal <anuragnepal99@gmail.com>
2025-03-28 12:39:36 +0545

    fix/SaveHall-availablity

--- src/main/java/com/example/BookEatNepal/Payload/Request/HallAvailabilityRequest.java
+++ src/main/java/com/example/BookEatNepal/Payload/Request/HallAvailabilityRequest.java
@@ -1,8 +1,6 @@
 package com.example.BookEatNepal.Payload.Request;
 import lombok.Builder;
 import lombok.Data;
-import java.sql.Time;
-import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -11,14 +9,14 @@ public class HallAvailabilityRequest {
         private String hallId;
         private String startDate;
         private String endDate;
-        private List<Shift> shift; // A list of Shift objects
+        private List<Shift> shift;
         private String status;
 
         @Data
-        @Builder// Apply Lombok to the inner class as well
+        @Builder
         public static class Shift {
             private String startTime;
             private String endTime;
-            private String shift; // Represents the shift name (e.g., Morning, Evening)
+            private String shift;
         }
-    }
+}
